662316b793a8db5a2af53d600b9bb7545ab4f545
DanDaManTran <DannDoesntKnow@Dans-MBP.attlocal.net>
2017-02-07 13:49:28 -0600

    fix the modal ouside click -DT

--- app/public/app.js
+++ app/public/app.js
@@ -78,11 +78,10 @@ $("document").ready(function(){
 		$("#myModal").css("display", "none");
 	});
 
-	// // When the user clicks anywhere outside of the modal, close it
-	// $(window).on("click", function(event){
-	// 	console.log(event.target);
-	// 	if (event.target == $("#myModal")){
-	// 		$("#myModal").css("display", "none");
-	// 	}
-	// });
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == document.getElementById('myModal')) {
+	        document.getElementById('myModal').style.display = "none";
+	    }
+	};
 });
